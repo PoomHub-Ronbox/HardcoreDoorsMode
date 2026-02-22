99d526d0f7c60fb5142d17418ce3680d4c808cf3
PoomHub-Ronbox <oo9176882@gmail.com>
2026-02-22 11:42:42 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,11 +1,15 @@
-local success, err = pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/localplayerr/Doors-stuff/refs/heads/main/Hardcore%20v4%20recreate/main%20code"))()
+task.spawn(function()
+    local success, err = pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/localplayerr/Doors-stuff/refs/heads/main/Hardcore%20v4%20recreate/main%20code"))()
+    end)
+    if not success then warn("Script 1 Error: " .. tostring(err)) end
 end)
 
-if not success then
-    warn("Script 1 failed to load: " .. tostring(err))
-end
+task.wait(1)
 
 task.spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/PoomHub-Ronbox/Sprinter/refs/heads/main/Main.txt"))()
+    local success, err = pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/PoomHub-Ronbox/Sprinter/refs/heads/main/Main.txt"))()
+    end)
+    if not success then warn("Script 2 Error: " .. tostring(err)) end
 end)
